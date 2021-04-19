af0d061be9c07aaeea83b6899cd0413a4d12c29f
Alaa Elnagar <35043356+AlaaElnagar@users.noreply.github.com>
2021-04-19 09:21:06 +0200

    Task_1

---
+++ PythonTask.py
@@ -0,0 +1,39 @@
+from Q1 import *
+
+Escape_char  = "start"
+
+while(Escape_char != "end"):
+    Prog_num=int(input("please enter numbre of Question from 1 to 16 "))
+
+    if (Prog_num) == 1 :
+        GetBirhDate()
+    elif Prog_num == 2 :
+        GetSumAndAverage()
+    elif Prog_num == 3 :
+        PyramidOfStars()
+    elif Prog_num == 4 :
+        EvenOrodd()
+    elif Prog_num == 5 :
+        RemVowels ()
+    elif Prog_num == 6 :
+        WriteListToFile()
+    elif Prog_num == 7 :
+        call_sum()
+    elif Prog_num == 8 :
+        EvenNumOFList()
+    elif Prog_num == 9 :
+        call_String()
+    elif Prog_num == 10 :
+        call_speed()
+    # elif Prog_num == 11 :
+    #
+    # elif Prog_num == 12:
+    #
+    # elif Prog_num == 13 :
+
+    elif Prog_num == 14 :
+         GetMaxNum()
+    elif Prog_num == 15 :
+         ReadFile()
+    elif Prog_num == 16 :
+         ParseFile()
